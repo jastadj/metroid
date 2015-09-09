8eaecfd7734fc154c6003e81cc2eefa84a370afa
John J <jastadj@gmail.com>
2015-09-09 15:49:58 -0700

    basic loop and tile loading

--- engine.cpp
+++ engine.cpp
@@ -4,9 +4,9 @@ Engine::Engine()
 {
 
     //init screen
-    screen = new sf::RenderWindow(sf::VideoMode(CHUNK_SIZE*SCREEN_WIDTH_CHUNKS,
+    m_Screen = new sf::RenderWindow(sf::VideoMode(CHUNK_SIZE*SCREEN_WIDTH_CHUNKS,
                                                 CHUNK_SIZE*SCREEN_HEIGHT_CHUNKS,32), "Metroid");
-
+    initTiles();
 
     mainLoop();
 }
@@ -16,17 +16,43 @@ Engine::~Engine()
 
 }
 
+bool Engine::initTiles()
+{
+    //load in texture sprite sheet
+    if(!m_TilesTXT.loadFromFile("tiles.png"))
+    {
+        std::cout << "Error loading tiles.png\n";
+        return false;
+    }
+
+    for(int i = 0; i < 3; i++)
+    {
+        sf::Sprite *newsprite = new sf::Sprite;
+        sf::IntRect srect;
+        srect.left = i*CHUNK_SIZE;
+        srect.top = 0;
+        srect.width = CHUNK_SIZE;
+        srect.height = CHUNK_SIZE;
+        newsprite->setTexture(m_TilesTXT);
+        newsprite->setTextureRect(srect);
+        newsprite->setScale( CHUNK_SCALE, CHUNK_SCALE);
+
+        m_TilesSPR.push_back(newsprite);
+    }
+
+}
+
 void Engine::mainLoop()
 {
     bool quit = false;
 
     while(!quit)
     {
-        screen->clear();
+        m_Screen->clear();
 
         sf::Event event;
 
-        while(screen->pollEvent(event) )
+        while(m_Screen->pollEvent(event) )
         {
             if(event.type == sf::Event::Closed) quit = true;
             else if(event.type == sf::Event::KeyPressed)
@@ -35,7 +61,8 @@ void Engine::mainLoop()
             }
         }
 
+        m_Screen->draw(*m_TilesSPR[0]);
 
-        screen->display();
+        m_Screen->display();
     }
 }
--- engine.hpp
+++ engine.hpp
@@ -2,17 +2,29 @@
 #define CLASS_ENGINE
 
 #include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
 #include <SFML\Graphics.hpp>
 
-#define CHUNK_SIZE 16
-#define SCREEN_WIDTH_CHUNKS 32
-#define SCREEN_HEIGHT_CHUNKS 28
+#define CHUNK_SIZE 8
+#define CHUNK_SCALE 2
+#define SCREEN_WIDTH_CHUNKS 32*CHUNK_SCALE
+#define SCREEN_HEIGHT_CHUNKS 28*CHUNK_SCALE
 
 class Engine
 {
 private:
 
-    sf::RenderWindow *screen;
+    sf::RenderWindow *m_Screen;
+
+    //init
+    bool initTiles();
+
+    //resources
+    sf::Texture m_TilesTXT;
+    std::vector< sf::Sprite* > m_TilesSPR;
+
 
     void mainLoop();
 
